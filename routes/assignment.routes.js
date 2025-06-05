9d1dd045f725b736223e8ad166182d584aa334ed
Ajinkya243 <ajinkyagund2@gmail.com>
2025-06-05 14:22:55 +0530

    feat: Add delete assignmnet api

--- routes/assignment.routes.js
+++ routes/assignment.routes.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const router=express.Router();
-const{addAssignment,getAllAssignments,getAssignmetById,updateAssignmentById, getAssignmetByProjectId, getAssignmentByUserId}=require("../controllers/assignment.controller");
+const{addAssignment,getAllAssignments,getAssignmetById,updateAssignmentById, getAssignmetByProjectId, getAssignmentByUserId, deleteAssignmentById}=require("../controllers/assignment.controller");
 
 router.get("/",getAllAssignments);
 router.post("/",addAssignment);
 router.get("/:id",getAssignmetById);
 router.post("/:id",updateAssignmentById);
+router.delete("/:id",deleteAssignmentById);
 router.get("/project/:id",getAssignmetByProjectId);
 router.get("/user/:id",getAssignmentByUserId);
 
